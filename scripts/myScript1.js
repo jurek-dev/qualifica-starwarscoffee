df37e8d443aaea33c7d88d5ca101d264b24222b6
jurek-dev <jurek.dev@outlook.com>
2022-05-27 17:45:36 -0300

    Adicionado música

--- scripts/myScript1.js
+++ scripts/myScript1.js
@@ -1,14 +1,17 @@
 var button = document.getElementById("play-theme").addEventListener("click", playMusic);
 var playingSound = false;
+var audio = new Audio('/musics/imperialmarch.mp3')
 
 function playMusic() {
     if(playingSound == false){
         playingSound = true;
+        audio.play();
         document.getElementById("onoff-text").textContent = "Parar música";
         document.getElementById("play-theme").className = "btn btn-danger";
     }
     else if(playingSound == true){
         playingSound = false;
+        audio.pause();
         document.getElementById("onoff-text").textContent = "Tocar música";
         document.getElementById("play-theme").className = "btn btn-warning";
     }
